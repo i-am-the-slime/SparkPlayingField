eefcf90958c15018f2554196dd7a7250456480b9
konrad <konrad.blaszkiewicz@gmail.com>
2014-05-16 19:59:34 +0200

    SparkPi now succeeds.

---
+++ src/main/scala/org/menthal/AppSessionAggregation.scala
@@ -0,0 +1,27 @@
+package org.menthal
+
+import org.apache.spark._
+
+/**
+ * Created by konrad on 16.05.2014.
+ */
+object AppSessionAggregation {
+
+  def main(args: Array[String]) {
+    if (args.length == 0) {
+      System.err.println("Usage: SimpleApp <master> [<slices>]")
+      System.exit(1)
+    }
+    val sc = new SparkContext(args(0), "SimpleApp",
+      System.getenv("SPARK_HOME"), SparkContext.jarOfClass(this.getClass))
+    val dumpFile = "/events.dump" // Should be some file on your system
+    val eventsDump = sc.textFile(dumpFile,2).cache()
+    val events = eventsDump.map(line => line.split("\t"))
+    val appSessions = events.filter(event => event(3) == "3000")
+
+    appSessions.saveAsTextFile("/results.txt")
+    //println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    sc.stop()
+
+  }
+}
--- src/main/scala/org/menthal/SimpleApp.scala
+++ src/main/scala/org/menthal/SimpleApp.scala
@@ -5,10 +5,10 @@ import org.apache.spark.SparkContext._
 object SimpleApp {
   def main(args: Array[String]) {
     if (args.length == 0) {
-      System.err.println("Usage: SparkPi <master> [<slices>]")
+      System.err.println("Usage: SimpleApp <master> [<slices>]")
       System.exit(1)
     }
-    val logFile = "/home/hduser/spark/README.md" // Should be some file on your system
+    val logFile = "/events.dump" // Should be some file on your system
     val sc = new SparkContext(args(0 ), "SimpleApp",
         System.getenv("SPARK_HOME"), SparkContext.jarOfClass(this.getClass))
     val logData = sc.textFile(logFile, 2).cache()
@@ -17,4 +17,5 @@ object SimpleApp {
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     sc.stop()
   }
+
 }
