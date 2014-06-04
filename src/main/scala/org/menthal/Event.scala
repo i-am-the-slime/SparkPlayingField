515b1896c3194a04d8ea36edf84ebcce27ad993e
konrad <konrad.blaszkiewicz@gmail.com>
2014-06-04 16:05:00 +0200

    Parsing events

--- src/main/scala/org/menthal/Event.scala
+++ src/main/scala/org/menthal/Event.scala
@@ -2,6 +2,7 @@ package org.menthal
 
 import org.joda.time.DateTime
 import spray.json._
+import scala.util.Try
 
 /**
  * Created by mark on 18.05.14.
@@ -10,16 +11,51 @@ sealed abstract class EventData(val eventType:Long)
 
 case class ScreenOff() extends EventData(Event.TYPE_SCREEN_OFF)
 case class ScreenUnlock() extends EventData(Event.TYPE_SCREEN_UNLOCK)
+case class DreamingStopped() extends EventData(Event.TYPE_DREAMING_STOPPED)
+case class DreamingStarted() extends EventData(Event.TYPE_DREAMING_STARTED)
+
 case class WindowStateChanged(appName:String, packageName:String, windowTitle:String)
   extends EventData(Event.TYPE_WINDOW_STATE_CHANGED)
 
 case class Event[A <: EventData](id:Long, userId:Long, data:A, time:DateTime) {
+
   override def toString:String = {
     val dataString = data.toString
     s"Event: id: $id, user: $userId, time: $time, data: $dataString)"
   }
 }
 object Event{
+  def tryToParseLine(dumpLine: String): Option[Event[_ <: EventData]] = {
+    val rawData = dumpLine.split("\t")
+    val event = for {
+          eventData <-  Try(getEventDataType(rawData(3), rawData(4)))
+          id <- Try(rawData(0).toLong)
+          userId <- Try(rawData(1).toLong)
+          time <- Try(DateTime.parse(rawData(2).replace(" ", "T")))
+          data <- Try(eventData.get)
+    } yield Some(Event[data.type](id, userId, data, time))
+    event getOrElse None
+  }
+
+   def getEventDataType(typeString:String, data:String):Option[EventData] = {
+     val typeNumber = typeString.toInt
+     typeNumber match {
+       case Event.TYPE_SCREEN_OFF =>
+         Some(ScreenOff())
+       case Event.TYPE_SCREEN_UNLOCK =>
+         Some(ScreenUnlock())
+       case Event.TYPE_DREAMING_STARTED =>
+         Some(DreamingStarted())
+       case Event.TYPE_DREAMING_STOPPED =>
+         Some(DreamingStopped())
+       case Event.TYPE_APP_SESSION =>
+         None
+       case Event.TYPE_WINDOW_STATE_CHANGED | Event.TYPE_WINDOW_STATE_CHANGE_BASIC =>
+         val d = data.parseJson.convertTo[List[String]]
+         Some(WindowStateChanged(d(0), d(1), d(2)))
+       case _ => None
+     }
+   }
 //  def fromJSON(json:String):Option[Event] = {
 //
 //    val parsed = json.parseJson.convertTo[Map[String, Int]]
@@ -29,7 +65,7 @@ object Event{
 //  val TYPE_VIEW_SELECTED = 4
 //  val TYPE_VIEW_FOCUSED = 8
 //  val TYPE_VIEW_TEXT_CHANGED = 16
-  val TYPE_WINDOW_STATE_CHANGED = 32
+  val TYPE_WINDOW_STATE_CHANGED: Int = 32
 //  val TYPE_NOTIFICATION_STATE_CHANGED = 64
 //  val TYPE_VIEW_HOVER_ENTER = 128
   val TYPE_WINDOW_STATE_CHANGE_BASIC = 132
