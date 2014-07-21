366a7b0433d2ec92c65a78f0f2671d9c08e3493e
i-am-the-slime <mark.eibes@gmail.com>
2014-07-21 12:24:46 +0200

    Created branch.

---
+++ src/test/scala/org/menthal/IOSpec.scala
@@ -0,0 +1,9 @@
+package org.menthal
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class IOSpec extends FlatSpec with Matchers {
+  "The IO class" should "read and write RDDs of AppSession" in {
+    
+  }
+}
