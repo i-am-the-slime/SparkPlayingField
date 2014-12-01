51280e2844c807b3160f6a577eff1c76e8788d12
i-am-the-slime <mark.eibes@gmail.com>
2014-12-01 22:33:02 +0100

    Before changing to Specs2

--- src/test/scala/org/menthal/PostgresDumpToParquetSpec.scala
+++ src/test/scala/org/menthal/PostgresDumpToParquetSpec.scala
@@ -43,7 +43,7 @@ class PostgresDumpToParquetSpec extends FlatSpec with Matchers with BeforeAndAft
     someEvent shouldBe expectedEvent
   }
 
-  "When given an input and output file" should "read the file and output the results for smss sent" in {
+  "When given an input and output file" should "read the file and output the results for SMS sent" in {
     PostgresDumpToParquet.parseFromDumpAndWriteToParquet(sc, inputPath , outputPath)
     val result:RDD[SmsSent] = ParquetIO.readEventType(outputPath, TYPE_SMS_SENT, sc)
     val someEvent = result.filter(smsRec => smsRec.getId == 192040L).take(1).toList(0)
@@ -52,7 +52,151 @@ class PostgresDumpToParquetSpec extends FlatSpec with Matchers with BeforeAndAft
     someEvent shouldBe expectedEvent
   }
 
-  "When given an dump file " should "read the file and output the results" in {
+  behavior of "Correct Parquet saving and loading from the dump file"
+
+  it should "work for WindowStateChanged" in {
+    type ET = WindowStateChanged
+    val event = new WindowStateChanged(194430L, 1L, 1369864301015L, "System Android", "android", "")
+    val typeNo = TYPE_WINDOW_STATE_CHANGED
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for NotificationStateChanged" in {
+    type ET = NotificationStateChanged
+    val event = new NotificationStateChanged(191100L, 154L, 1369866730909L, "Google Search",
+      "com.google.android.googlequicksearchbox", 2L)
+    val typeNo = TYPE_NOTIFICATION_STATE_CHANGED
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for SMS Received" in {
+    type ET = SmsReceived
+    val event = new SmsReceived(194660L, 2L, 1369880442744L,
+      "6fa882592487c294b76b86b315ac9276bbcb924b93af8e40f73fde9044c23850dd20fe25068b5ef9156480c9b7fe63ff67b25ba984331cc26fc2658bd2382e8d",
+       135)
+    val typeNo = TYPE_SMS_RECEIVED
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for SMS Sent" in {
+    type ET = SmsSent
+    val event = new SmsSent(192040L, 1L, 1369880442744L,
+      "676962b809fac8b6cb64113f6ee3bc594ca3e7b59cb35863c15dd69f668b7763131e0c9a7708f5d9201e1e64783ac6eeb14c36b382bf7da14042575c54230f46",
+      125)
+    val typeNo = TYPE_SMS_SENT
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Call Received" in {
+    type ET = CallReceived
+    val event = new CallReceived(192040L, 154L, 1369880442744L,
+      "613a016635fc407c1b3a1b6122f2469aac080a0b3ab37110e0d92d2be5d84acda400260ffc7fc771b16af49588babd2f326eb61c5ad903a499693ba36a9e5651",
+      1369854866723L, 64L)
+    val typeNo = TYPE_CALL_RECEIVED
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+
+  it should "work for Call Outgoing" in {
+    type ET = CallOutgoing
+    val event = new CallOutgoing(190829L, 154L, 1369880442744L,
+      "613a016635fc407c1b3a1b6122f2469aac080a0b3ab37110e0d92d2be5d84acda400260ffc7fc771b16af49588babd2f326eb61c5ad903a499693ba36a9e5651",
+      1369852973032L, 43L)
+    val typeNo = TYPE_CALL_OUTGOING
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Call Missed" in {
+//    type ET = CallMissed
+//    val event = new CallMissed(192040L, 1L, 1369880442744L,
+//      "676962b809fac8b6cb64113f6ee3bc594ca3e7b59cb35863c15dd69f668b7763131e0c9a7708f5d9201e1e64783ac6eeb14c36b382bf7da14042575c54230f46",
+//      125)
+//    val typeNo = TYPE_CALL_MISSED
+//
+//    //Always the same
+//    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+//    result shouldBe event
+    "we need one example of this" shouldBe ""
+  }
+
+  it should "work for Screen On" in {
+    type ET = ScreenOn
+    val event = new ScreenOn(194670L, 2L, 1369880442744L)
+    val typeNo = TYPE_SCREEN_ON
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Screen Off" in {
+    type ET = ScreenOff
+    val event = new ScreenOff(190840L, 154L, 1369880442744L)
+    val typeNo = TYPE_SCREEN_OFF
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Screen Unlock" in {
+    type ET = ScreenUnlock
+    val event = new ScreenUnlock(194671L, 2L, 1369880442744L)
+    val typeNo = TYPE_SCREEN_UNLOCK
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Localisation (GPS)" in {
+    type ET = Localisation
+    val event = new Localisation(194570L, 154L, 1369880442744L, "gps", 10.0f, 7.12154483, 50.735962)
+    val typeNo = TYPE_LOCALISATION
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for AppInstall" in {
+    type ET = AppInstall
+    val event = new AppInstall(195669L, 2L, 1369880442744L,"Nyx", "com.menthal.nyx")
+    val typeNo = TYPE_APP_INSTALL
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for Mood" in {
+    type ET = Mood
+    val event = new Mood(192040L, 1L, 1369880442744L, 4.0f)
+    val typeNo = TYPE_MOOD
+
+    //Always the same
+    val result = writeAndGet[ET](typeNo, filter = _.getId == event.getId )
+    result shouldBe event
+  }
+
+  it should "work for DreamingStarted" in {
     PostgresDumpToParquet.parseFromDumpAndWriteToParquet(sc, inputPath , outputPath)
     val result:RDD[DreamingStarted] = ParquetIO.readEventType(outputPath, TYPE_DREAMING_STARTED, sc)
     val someDreamingEvent = result.filter(dreamRec => dreamRec.getId == 1910171L).take(1).toList(0)
@@ -60,37 +204,31 @@ class PostgresDumpToParquetSpec extends FlatSpec with Matchers with BeforeAndAft
     someDreamingEvent shouldBe expectedDreamingEvent
   }
 
-  "When given an dump file " should "read the Dreaming stopped events and write them to Parquet correctly" in {
-    val eventType =TYPE_DREAMING_STOPPED
+  it should "work for DreamingStopped" in {
+    val eventType = TYPE_DREAMING_STOPPED
     val filter = (dreamRec:DreamingStopped) => dreamRec.getId == 1910172L
     val expectedDreamingEvent = new DreamingStopped(1910172L, 154L, 1369858030111L)
-    parquetTest[DreamingStopped](eventType, filter) shouldBe expectedDreamingEvent
+    writeAndGet[DreamingStopped](eventType, filter) shouldBe expectedDreamingEvent
   }
 
-  "When given an dump file " should "read the AppRemoval events and write them to Parquet correctly "  in {
-    val eventType =TYPE_APP_REMOVAL
+  it should "work for AppRemoval" in {
+    val eventType = TYPE_APP_REMOVAL
     val filter = (ev:AppRemoval) => ev.getId == 3045489513L
-    val expectedEvent = new AppRemoval(3045489513L, 8912L, 1369858030111L, "YouTube", "com.google.android.youtube")
-    parquetTest[AppRemoval](eventType, filter) shouldBe expectedEvent
+    val expectedEvent = new AppRemoval(3045489513L, 8912L, 1415938023906L, "YouTube", "com.google.android.youtube")
+    writeAndGet[AppRemoval](eventType, filter) shouldBe expectedEvent
   }
 
-  "When given an dump file " should "read the TrafficData events and write them to Parquet correctly "  in {
-    val eventType =TYPE_TRAFFIC_DATA
+  it should "work for TrafficData" in {
+    val eventType = TYPE_TRAFFIC_DATA
     val filter = (ev:TrafficData) => ev.getId == 2149587751L
-    val expectedEvent = new TrafficData(2149587751L, 42535L, 1407583604729L, 1, "FRITZ!Box\t6360\tCable", 0, 780327L, 102914L, 0L)
-    parquetTest[TrafficData](eventType, filter) shouldBe expectedEvent
+    val expectedEvent =
+      new TrafficData(2149587751L, 42535L, 1407583604729L, 1, "\"FRITZ!Box	6360	Cable\"", 0, 780327L, 102914L, 0L)
+    writeAndGet[TrafficData](eventType, filter) shouldBe expectedEvent
   }
 
-
-
-
-  def parquetTest[A <: SpecificRecord](eventType: Int, filter: A => Boolean)(implicit cd : ClassTag[A]):A = {
+  def writeAndGet[A <: SpecificRecord](eventType: Int, filter: A => Boolean) (implicit ct : ClassTag[A]):A = {
     PostgresDumpToParquet.parseFromDumpAndWriteToParquet(sc, inputPath, outputPath)
     val result:RDD[A] = ParquetIO.readEventType[A](outputPath, eventType, sc)
-    info("\n\n" + result.count().toString +  "\n\n")
     result.filter(filter).take(1).toList(0)
   }
-
-
-
 }
